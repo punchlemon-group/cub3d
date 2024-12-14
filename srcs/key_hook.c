c1e17c64dc58a1da874218ab9207db8d38f8f9c2
punchlemon <tsukiwahigashini0410@gmail.com>
2024-12-14 17:42:16 +0900

    print player status

--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 16:18:03 by retanaka          #+#    #+#             */
-/*   Updated: 2024/12/14 17:27:01 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/12/14 17:39:14 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int	key_hook(int keycode, t_vars *vars)
 			vars->player.angle_deg--;
 		vars->player.angle_rad = vars->player.angle_deg * PI / 180;
 	}
+	printf("x:%f, y:%f, angle_deg:%d, angle_rad:%f\n", vars->player.x, vars->player.y, vars->player.angle_deg, vars->player.angle_rad);
 	return (CNT);
 }
