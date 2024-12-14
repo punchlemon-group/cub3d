595208db5c32017eec2040de2f8a88279e22b0b3
punchlemon <tsukiwahigashini0410@gmail.com>
2024-12-14 19:35:37 +0900

    同時押し W&S A&Dに対応

--- srcs/loop_hook.c
+++ srcs/loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 18:31:19 by retanaka          #+#    #+#             */
-/*   Updated: 2024/12/14 19:01:55 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/12/14 19:31:21 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,29 @@ int	loop_hook(t_vars *vars)
 {
 	int	bias;
 
-	if (vars->keys[W_ID] || vars->keys[S_ID]
-		|| vars->keys[D_ID] || vars->keys[A_ID])
+	if (vars->keys[W_ID] || vars->keys[S_ID])
 	{
 		if (vars->keys[W_ID])
 			bias = 90;
-		else if (vars->keys[A_ID])
-			bias = 180;
-		else if (vars->keys[S_ID])
+		if (vars->keys[S_ID])
 			bias = 270;
-		else if (vars->keys[D_ID])
+		if (!(vars->keys[W_ID] && vars->keys[S_ID]))
+		{
+			vars->player.x += 0.01 * cos(vars->player.angle_rad + bias * PI / 180);
+			vars->player.y -= 0.01 * sin(vars->player.angle_rad + bias * PI / 180);
+		}
+	}
+	if (vars->keys[A_ID] || vars->keys[D_ID])
+	{
+		if (vars->keys[A_ID])
+			bias = 180;
+		if (vars->keys[D_ID])
 			bias = 0;
-		vars->player.x += 0.01 * cos(vars->player.angle_rad + bias * PI / 180);
-		vars->player.y -= 0.01 * sin(vars->player.angle_rad + bias * PI / 180);
+		if (!(vars->keys[A_ID] && vars->keys[D_ID]))
+		{
+			vars->player.x += 0.01 * cos(vars->player.angle_rad + bias * PI / 180);
+			vars->player.y -= 0.01 * sin(vars->player.angle_rad + bias * PI / 180);
+		}
 	}
 	if (vars->keys[RIGHT_ID] || vars->keys[LEFT_ID])
 	{
