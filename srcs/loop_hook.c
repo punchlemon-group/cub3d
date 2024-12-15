d1a6556fdcab9ffdc7d001c18636fe3e5959b442
punchlemon <tsukiwahigashini0410@gmail.com>
2024-12-15 11:01:14 +0900

    simplification of input judgment logic

--- srcs/loop_hook.c
+++ srcs/loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 18:31:19 by retanaka          #+#    #+#             */
-/*   Updated: 2024/12/15 10:44:54 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/12/15 10:57:48 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,25 @@ int	loop_hook(t_vars *vars)
 	long			tmp;
 	int				bias;
 
-	if (vars->keys[W_ID] || vars->keys[S_ID])
+	if (vars->keys[W_ID] != vars->keys[S_ID])
 	{
 		if (vars->keys[W_ID])
 			bias = 90;
 		if (vars->keys[S_ID])
 			bias = 270;
-		if (!(vars->keys[W_ID] && vars->keys[S_ID]))
-		{
-			vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
-			vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
-		}
+		vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
+		vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
 	}
-	if (vars->keys[A_ID] || vars->keys[D_ID])
+	if (vars->keys[A_ID] != vars->keys[D_ID])
 	{
 		if (vars->keys[A_ID])
 			bias = 180;
 		if (vars->keys[D_ID])
 			bias = 0;
-		if (!(vars->keys[A_ID] && vars->keys[D_ID]))
-		{
-			vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
-			vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
-		}
+		vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
+		vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
 	} // これだと前と横同時押しされたときに√2倍速く進む
-	if (vars->keys[RIGHT_ID] || vars->keys[LEFT_ID])
+	if (vars->keys[RIGHT_ID] != vars->keys[LEFT_ID])
 	{
 		if (vars->keys[RIGHT_ID])
 			vars->player.angle_rad -= 0.0001;
@@ -57,14 +51,14 @@ int	loop_hook(t_vars *vars)
 	gettimeofday(&tv, NULL);
 	tmp = tv.tv_sec * 1000000 + tv.tv_usec;
 	if (vars->last_calc_time)
-		printf("%lfHz\n", 1000000.0 / (tmp - vars->last_calc_time));
+		// printf("%lfHz\n", 1000000.0 / (tmp - vars->last_calc_time));
 	vars->last_calc_time = tmp;
 	if (!vars->last_disp_time || (tmp - vars->last_disp_time) > 1000000 / FPS)
 	{
 		vars->last_disp_time = tmp;
-		if (vars->keys[W_ID] || vars->keys[S_ID]
-			|| vars->keys[A_ID] || vars->keys[D_ID]
-			|| vars->keys[RIGHT_ID] || vars->keys[LEFT_ID])
+		if ((vars->keys[W_ID] != vars->keys[S_ID])
+			|| (vars->keys[A_ID] != vars->keys[D_ID])
+			|| (vars->keys[RIGHT_ID] != vars->keys[LEFT_ID]))
 			print_player_status(vars->player);
 		draw_player_2d(vars, 0xff0000);
 	}
