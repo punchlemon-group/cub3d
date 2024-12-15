0ee4e7892567b940910ea602eb09674b2a6e3aa6
punchlemon <tsukiwahigashini0410@gmail.com>
2024-12-15 11:12:59 +0900

    add function 'player_move' resolve #3

--- srcs/loop_hook.c
+++ srcs/loop_hook.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 18:31:19 by retanaka          #+#    #+#             */
-/*   Updated: 2024/12/15 10:57:48 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/12/15 11:11:45 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,41 @@ void	print_player_status(t_player player)
 	printf("x:%f, y:%f, angle_rad:%f\n", player.x, player.y, player.angle_rad);
 }
 
-int	loop_hook(t_vars *vars)
+void	player_move(t_vars *vars)
 {
-	struct timeval	tv;
-	long			tmp;
-	int				bias;
+	float	speed;
+	int		bias;
 
+	bias = 0;
+	speed = 0.0001;
 	if (vars->keys[W_ID] != vars->keys[S_ID])
 	{
 		if (vars->keys[W_ID])
-			bias = 90;
-		if (vars->keys[S_ID])
-			bias = 270;
-		vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
-		vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
+			bias += 90;
+		else if (vars->keys[S_ID])
+			bias += 270;
 	}
 	if (vars->keys[A_ID] != vars->keys[D_ID])
 	{
 		if (vars->keys[A_ID])
-			bias = 180;
-		if (vars->keys[D_ID])
-			bias = 0;
-		vars->player.x += 0.0001 * cos(vars->player.angle_rad + bias * PI / 180);
-		vars->player.y -= 0.0001 * sin(vars->player.angle_rad + bias * PI / 180);
-	} // これだと前と横同時押しされたときに√2倍速く進む
+			bias += 180;
+		if (vars->keys[W_ID] != vars->keys[S_ID])
+			bias /= 2;
+	}
+	if (vars->keys[W_ID] != vars->keys[S_ID]
+		|| vars->keys[A_ID] != vars->keys[D_ID])
+	{
+		vars->player.x += speed * cos(vars->player.angle_rad + bias * PI / 180);
+		vars->player.y -= speed * sin(vars->player.angle_rad + bias * PI / 180);
+	}
+}
+
+int	loop_hook(t_vars *vars)
+{
+	struct timeval	tv;
+	long			tmp;
+
+	player_move(vars);
 	if (vars->keys[RIGHT_ID] != vars->keys[LEFT_ID])
 	{
 		if (vars->keys[RIGHT_ID])
@@ -51,7 +62,7 @@ int	loop_hook(t_vars *vars)
 	gettimeofday(&tv, NULL);
 	tmp = tv.tv_sec * 1000000 + tv.tv_usec;
 	if (vars->last_calc_time)
-		// printf("%lfHz\n", 1000000.0 / (tmp - vars->last_calc_time));
+		printf("%lfHz\n", 1000000.0 / (tmp - vars->last_calc_time));
 	vars->last_calc_time = tmp;
 	if (!vars->last_disp_time || (tmp - vars->last_disp_time) > 1000000 / FPS)
 	{
